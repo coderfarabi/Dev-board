3782d04b77db8c3eeac915be48fc1d2f80b49fac
aar <coderaarfarabi@gmail.com>
2025-06-16 11:30:11 +0600

    add real-time functionality and task management features

--- script/index.js
+++ script/index.js
@@ -1,4 +1,27 @@
-setInterval( updatedDate, 1000);
+document.addEventListener("DOMContentLoaded", () => {
+  // clicking on the logo/home button
+  document.getElementById("brand-logo").addEventListener("click", () => {
+    window.location.href = "/html/index.html";
+  });
 
+  // clicking on the "Discover Today" button
+  document.getElementById("discover-today").addEventListener("click", () => {
+    window.location.href = "/html/discover-today.html";
+  });
+  const taskCard = document.querySelectorAll(".task_card");
+  let taskCount = taskCard.length;
+  // console.log(taskCount);
+  document.getElementById("task_assigned").innerText = taskCount;
 
+  document.querySelectorAll(".completed-button").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      document.querySelector(".completed-button").classList.add("disabled");
+      alert("Board updated successfully!");
+      document.getElementById("task_assigned").innerText --;
+      document.getElementById("task_completed").innerText ++;
+    });
+  });
+});
 
+// real-time date
+setInterval(updatedDate, 1000);
